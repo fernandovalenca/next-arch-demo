c7d0866881e98ced14083ee1f7eaa0d716bb7b6d
Fernando Valença <fernandovalenca2013@gmail.com>
2026-01-31 16:53:17 -0300

    chore(next): configura standalone mode no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
-};
+  output: 'standalone',
+} satisfies NextConfig;
 
 export default nextConfig;
